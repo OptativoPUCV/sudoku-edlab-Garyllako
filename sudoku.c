626af0ffbfb4e5551aa1ad787db8c8a59bd3248b
repl.it user <replituser@example.com>
2020-10-15 02:08:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,6 +50,19 @@ int is_valid(Node* n){
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
+    int i,j,k;
+    for(i=0; i<9 ;i++){
+      for(j=0; j<9 ;j++){
+        if(n->sudo[i][j]==0){
+          for(k=1; k<=9 ;k++){
+            Node * nuevo = copy(n);
+            pushBack(list,nuevo);
+          }
+          n->sudo[i][j]=0;
+          return list;
+        }
+      }
+    }
     return list;
 }
 
