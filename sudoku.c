7ce0b7c5bb6797ee82d48c93e94466c903a2a053
repl.it user <replituser@example.com>
2020-10-16 00:37:48 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,20 +43,7 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  int arreglo[10] = {};
-  int i,j;
-  for(i=0; i<9 ;i++){
-    for(j=0; j<9 ;j++){
-      if(n->sudo[i][j] != 0){
-        arreglo[i] = 1;
-      }
-      else{
-        return 0;
-      }
-      printf("%d",arreglo[i]);
-    }
-    printf("\n");
-  }
+  
   return 1;
 }
 
