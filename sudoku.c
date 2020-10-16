60fb335b9112f2f8f1f5019b6654be82d0863035
repl.it user <replituser@example.com>
2020-10-16 00:51:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -71,6 +71,7 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
+  /*
     int i,j;
     for(i=0; i<9 ;i++){
       for(j=0; j<9 ;j++){
@@ -86,7 +87,7 @@ int is_final(Node* n){
           }
         }
       }
-    }
+    }*/
     return 1;
 }
 
