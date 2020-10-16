7a7893086e76bc0b89d60e326a8215b17cb6a337
repl.it user <replituser@example.com>
2020-10-16 14:10:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -75,7 +75,6 @@ int is_final(Node* n){
     int i,j;
     for(i=0; i<9 ;i++){
       for(j=0; j<9 ;j++){
-        if(n != NULL){
           if(n->sudo[i][j] == 0){
             return 0;
           }
@@ -84,7 +83,6 @@ int is_final(Node* n){
               return 0;
             }
           }
-        }
       }
     }
     return 1;
