17cd424206c634b6a3c345122b16452aef96750e
repl.it user <replituser@example.com>
2020-10-17 21:19:33 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,10 +43,17 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-   return(1);
+    int i,j;
+    for(i=0; i<9 ;i++){
+      for(j=0; j<9 ;j++){
+        printf("%d",n->sudo[i][j]);
+      }
+    }
+
+
+
+   return 1;
 }
-
-
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
