c8d70bb3fbddc18a034ef68bf8312a914643689b
repl.it user <replituser@example.com>
2020-10-18 16:44:37 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -80,8 +80,8 @@ int is_valid(Node* n){
               z = 3*(t%3) + (g%3);
               if(arreglo[k][z] == n->sudo[i][j]){
                 if((k != i) && (z != j)){
-                  printf("%d ",arreglo[k][z]);
-                  printf("%d ",n->sudo[i][j]);
+                  printf("%d %d ",k,z);
+                  printf("%d %d ",i,j);
                   if(p%3 == 2) printf("\n");
                   return 0;
                 }
